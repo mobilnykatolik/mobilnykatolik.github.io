590a7bd81fbf42c215ed00e033136d14826b993e
mobilnykatolik <mobilnykatolik@gmail.com>
2025-06-17 10:43:19 +0200

    upd

---
+++ app/exemptions.js
@@ -0,0 +1,42 @@
+function newExemption() {
+    window.open('#newexemptionapp', '_self')
+    document.getElementById("newExemption1").value = ""
+    document.getElementById("newExemption2").value = ""
+    document.getElementById("newExemption3").value = ""
+    document.getElementById("newExemption4").value = ""
+}
+
+function sendExemptionAsk() {
+    var data = {
+        "userid": userID,
+        "eventID": document.getElementById("newExemption1").value,
+        "date": document.getElementById("newExemption2").value,
+        "start": document.getElementById("newExemption3").value,
+        "end": document.getElementById("newExemption4").value,
+    }
+    console.log(data);
+    var url = `https://api.mobilnykatolik.pl/pmk/exemptions/new`;
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status == 200) {
+                history.back()
+                window.alert("Wysłano prośbę do opiekuna SU!")
+                return false;
+            } else {
+                window.alert("Wystąpił błąd. Spróbuj ponownie!");
+                return false;
+            }
+        }
+    };
+    xhr.send(JSON.stringify(data));
+    return false;
+}
+
+function openExemption(exID) {
+    window.alert("Funkcja jest aktualnie niedostępna!")
+}
---
+++ app/icsu.js
@@ -0,0 +1,85 @@
+var exDB = {}
+var waitingReviews = []
+
+const exStatus = ["OCZEKUJĄCE", "AKTYWNE", "ODRZUCONE", "ZARCHIWIZOWANE"]
+const exColor = ["warning", "success", "danger", "secondary"]
+
+function updateICSU() {
+    resetDigest()
+    var url = `https://api.mobilnykatolik.pl/icsu/${loginid}`;
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            let icsu = JSON.parse(xhr.responseText)
+            waitingReviews = icsu.waitingReviews
+            exDB = icsu.exemptions
+
+            //Oczekujące sprawozdania
+            if (waitingReviews.length > 0) {
+                document.getElementById("icsu-indicator-warning").style.display = "block"
+            } else {
+                document.getElementById("icsu-indicator-success").style.display = "block"
+            }
+
+            //Zwolnienia
+            var exCount = 0
+            if (Object.keys(exDB).length > 0) {
+                document.getElementById("icsu-no-exemptions").style.display = "none"
+                document.getElementById("icsu-exemptions").style.display = "block"
+            }
+            for (ex in exDB) {
+                exCount += 1
+                if (exCount == 1 && Object.keys(exDB).length != 1) {
+                    document.getElementById("icsu-exemptions").innerHTML += `
+                    <div type="top" onClick="openExemption('${ex}')">
+                        <span>
+                            Zwolnienie
+                            <br><i>${exDB[ex].date}</i>
+                            <container class="${exColor[exDB[ex].status]}">${exStatus[exDB[ex].status]}</container>
+                        </span>
+                    </div>`
+                } else if (exCount == Object.keys(exDB).length && Object.keys(exDB).length != 1) {
+                    document.getElementById("icsu-exemptions").innerHTML += `
+                    <div type="bottom" onClick="openExemption('${ex}')">
+                        <span>
+                            Zwolnienie
+                            <br><i>${exDB[ex].date}</i>
+                            <container class="${exColor[exDB[ex].status]}">${exStatus[exDB[ex].status]}</container>
+                        </span>
+                    </div>`
+                } else if (Object.keys(exDB).length == 1) {
+                    document.getElementById("icsu-exemptions").innerHTML += `
+                    <div type="ind" onClick="openExemption('${ex}')">
+                        <span>
+                            Zwolnienie
+                            <br><i>${exDB[ex].date}</i>
+                            <container class="${exColor[exDB[ex].status]}">${exStatus[exDB[ex].status]}</container>
+                        </span>
+                    </div>`
+                } else {
+                    document.getElementById("icsu-exemptions").innerHTML += `
+                    <div onClick="openExemption('${ex}')">
+                        <span>
+                            Zwolnienie
+                            <br><i>${exDB[ex].date}</i>
+                            <container class="${exColor[exDB[ex].status]}">${exStatus[exDB[ex].status]}</container>
+                        </span>
+                    </div>`
+                }
+            }
+
+        }
+    };
+    xhr.send();
+}
+
+function resetDigest() {
+    document.getElementById("icsu-indicator-success").style.display = "none"
+    document.getElementById("icsu-indicator-warning").style.display = "none"
+    document.getElementById("icsu-no-projects").style.display = "block"
+    document.getElementById("icsu-no-exemptions").style.display = "block"
+    document.getElementById("icsu-exemptions").style.display = "none"
+    document.getElementById("icsu-exemptions").innerHTML = ""
+}
