d640cbbbe4d0aec5cd0b3129879d035b88635784
mobilnykatolik <mobilnykatolik@gmail.com>
2025-06-18 07:46:52 +0200

    upd

--- app/exemptions.js
+++ app/exemptions.js
@@ -10,8 +10,6 @@ function sendExemptionAsk() {
     if (!window.confirm("Czy na pewno chcesz wysłać tę prośbę o zwolnienie?")) {
         return false;
     }
-    window.alert("Moduł Zwolnienia jest wyłączony!")
-    return false;
     var data = {
         "userid": userID,
         "eventID": document.getElementById("newExemption1").value,
